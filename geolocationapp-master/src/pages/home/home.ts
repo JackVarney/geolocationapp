fbdc381d5c1c90e9c7e072d1e3c66c86799789cb
JackVarney <jack.varney@iosh.com>
2017-07-29 19:04:57 +0100

    first commit

---
+++ geolocationapp-master/src/pages/home/home.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { } from '@types/googlemaps';
+
+
+
+// I commented this so you better be looking at it
+
+var latitude = 0;
+var longitude = 0;
+var postCode;
+var thisString = "I am a string";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+
+})
+
+
+
+export class HomePage {  
+
+  //  https://stackoverflow.com/questions/34714462/updating-variable-changes-in-components-from-a-service-with-angular2
+  appPostCode: string = thisString;
+
+  constructor(public navCtrl: NavController) {
+  }  
+
+  initMap(e) {
+
+    var map;
+    
+    var weather;
+
+    // this method gets the longitude and latitude
+
+    function showMap(position) {
+
+      //I would like to perform the below 2 lines of code in a seperate function however I couldnt figure out how to
+      longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+
+      console.log(longitude, latitude);
+
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: latitude, lng: longitude },
+        zoom: 15
+      });
+      getPostCode();
+      getWeather();
+
+    }
+
+    function dontShowMap() {
+      //something to tell them theyve been naughty not allowing me to see wheere i am
+    }
+
+    navigator.geolocation.getCurrentPosition(showMap, dontShowMap);
+
+
+    // gets postcode based upon lat+lon from postcodes.io or postcode.io
+    // didnt think i could get the weather from lon and lat so i did this first, didnt actually need to do it
+    function getPostCode() {
+      var req = new XMLHttpRequest()
+      req.open("GET", "http://api.postcodes.io/postcodes?lon=" + longitude + "&lat=" + latitude, false);
+      req.send(null);
+
+      var r = JSON.parse(req.response);
+      postCode = r.result[0].postcode;
+      console.log("postcode: " + postCode);    
+
+      thisString = postCode.toString();
+
+      console.log("thisString: " + thisString);
+    }
+
+    // gets weather based upon lon+lat from openweather
+    function getWeather() {
+
+      var req = new XMLHttpRequest()
+      req.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=7f6c7a16080ef8a18af35d11d9a891bb", false);
+      req.send(null);
+
+      var r = JSON.parse(req.response);
+      var weather = r.weather[0].main;
+      console.log("weather is:" + weather);
+    }   
+  }  
+}
